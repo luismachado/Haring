7d1bdf948887b4700f60d1166a829f760c7edab0
Luis Machado <luismachado@users.noreply.github.com>
2021-12-14 13:23:41 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,6 +16,9 @@ let package = Package(
         )
     ],
     targets: [
-        .target(name: "Haring")
+        .target(
+            name: "Haring",
+            path: "Haring/Classes"
+        )
     ]
 )
