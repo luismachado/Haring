359bc64c36a277e5b6abbeaea93cd2061347087c
Luis Machado <luismachado@users.noreply.github.com>
2021-12-14 13:12:23 +0000

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "Haring",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v9),
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Haring",
+            targets: ["Haring"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Haring",
+            path: "Haring/Sources"
+        )
+    ]
+)
